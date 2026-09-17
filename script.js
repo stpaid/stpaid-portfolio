855fbcda8632be03ae4d4981f3f72af7e6756945
stpaid <saintqud@gmail.com>
2026-09-17 22:44:48 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("STPAID Portfolio Loaded");
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(a=>{
+a.addEventListener('click',e=>{
+e.preventDefault();
+document.querySelector(a.getAttribute('href')).scrollIntoView({behavior:'smooth'});
+});
+});
